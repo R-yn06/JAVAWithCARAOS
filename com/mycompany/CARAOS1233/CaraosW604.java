d337d8e641524709f78c20ca06268a6d85cbd4fa
Ryan <rcaraos@addu.edu.ph>
2026-02-24 21:54:54 +0800

    yes

---
+++ com/mycompany/CARAOS1233/CaraosW604.java
@@ -0,0 +1,36 @@
+package com.mycompany.CARAOS1233;
+
+import java.util.Scanner;
+public class CaraosW604 {
+    public static void main (String[] args){
+        int n = 0;
+        int odd = 0;
+        int even = 0;
+        int zeros = 0;
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("How many integers to input:");
+        n = sc.nextInt();
+        for (int i = 0; n >= i; i++){
+            System.out.println ("Input an integer");
+            int num = sc.nextInt();
+
+            if (num == 0){
+                zeros++;
+            }
+            else if (num % 2 == 0){
+                even++;
+            }
+            else {
+                odd++;
+            }
+            }
+            sc.close();
+            
+        System.out.println("Number of odd integers: " + odd);
+        System.out.println("Number of even integers: " + even);
+        System.out.println("Number of zeros: " + zeros);
+        }
+        
+    }
+
---
+++ com/mycompany/CARAOS1233/CaraosW605.java
@@ -0,0 +1,8 @@
+package com.mycompany.CARAOS1233;
+
+import java.util.Scanner;
+public class CaraosW605 {
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+     
+}
